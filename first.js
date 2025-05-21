806d86129b58411970034731bd9f354ec4df2f24
Dhruv Malaviya <dhruvmalaviya14@gmail.com>
2025-05-21 23:56:41 +0530

    payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,8 @@ console.log(button);
 const login = ";pgin page added";
 console.log(login);
 
+
 const footer = "footer added in our website";
 console.log(footer);
+const payment = "integrated the payment gateway";
+console.log(payment);
